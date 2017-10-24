c1b06434c5967e52ed292ad3ba093ac6749b6c2d
Sam Tobin-Hochstadt <samth@ccs.neu.edu>
2017-10-23 19:29:57 -0700

    up

--- main.rkt
+++ main.rkt
@@ -58,7 +58,7 @@
 
 (play-n #:layout 'center
         #:skip-first? #t
-        (lambda (ts flow titan unsound)
+        (λ (ts flow titan unsound)
           (ppict-do full-page
            #:go (coord .5 .5 'cc)
            (cellophane (scale (bitmap "typescript.png") .9) ts)
@@ -70,6 +70,8 @@
            (if (= unsound 1)
                (rotate (shadow-frame (t/cant "All Unsound!" size1)) .5)
                (blank 1)))))
+
+(tslide "But Why?")
 
 (define (quoted author ts)
   (vr-append (apply vl-append (for/list ([t ts])
@@ -91,22 +93,82 @@
                  "typed code.")))
 
 
+(slide (quoted "John Clements, ..."
+               '()))
+
+
 (pslide
  #:go (coord .5 .0 'ct)
  (scale (page->pict "popl16-tfgnvf.pdf") 1.8)
  #:next
  #:go (coord .5 .5 'cc)
  (shadow-frame
-  (vl-append (t/cant "The problem is that, according to our measurements," size3)
-             (t/cant "the cost of enforcing soundness is overwhelming." size3))))
+  (vl-append
+   (t/cant "The problem is that, according to our measurements,"
+           size3)
+   (t/cant "the cost of enforcing soundness is overwhelming."
+           size3)))
+ #:next
+ #:go (coord .5 .5 'cc)
+ (shadow-frame
+  (scale (bitmap "synth.png") .8))
+ #:go (coord .5 .5 'cc)
+ #:next
+ (shadow-frame
+  (cc-superimpose
+   (blank 1008 450)
+   (apply vl-append
+          (for/list ([t '("We find that Typed Racket’s cost of"
+                          "soundness is not tolerable. If applying"
+                          "our method to other gradual type system"
+                          "implementations yields similar results,"
+                          "then sound gradual typing is dead.")])
+            
+            (t/cant t size3)))))
+
+ )
 
 
 
 
 (pic "princess.jpg")
 
+(tslide "Enter Pycket")
 
-(code-colorize-enabled #t)
+(pic "aggregate.png" .8)
+
+(slide #:title "Synth, again"
+       #:layout 'center
+       (bitmap (scale (page->pict "slowdown-synth-warmup-0.pdf") 1.4)))
+
+(slide #:title (t/quat "Two Key Ideas" size2)
+ (item (t/cant "Tracing JIT Compilation" size2))
+ (blank 125)
+ (item (t/cant "Hidden Classes for Chaperones" size2)))
+
+;(code-colorize-enabled #t)
+
+(slide #:layout 'center
+ (code (define f (cast #,(red-code (λ (x) (+ x 1)))
+                       (-> Integer Integer))))
+ 'next!
+ (arrow 30 (- (/ pi 2)))
+ 'alts
+ (list
+  (list
+   (code (define (f x*)
+           (cast (#,(red-code (λ (x) (+ x 1))) (cast x* Integer))
+                 Integer))))
+  (list
+   (code
+    (define f* (checked-fn #,(red-code (λ (x) (+ x 1)))
+                          Integer Integer))
+    (define (f x*)
+      (cast ((checked-fn-call f)
+             (cast x* (checked-fn-domain f)))
+            (checked-fn-range f)))))))
+   
+
 ;(slide (langs-pict #t))
 
 (define (titlet s)
